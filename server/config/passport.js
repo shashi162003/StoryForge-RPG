569cdf5a5cb00a8c7406a678652ed82b76e64487
masterShashi <shashikumargupta443@gmail.com>
2025-10-02 15:04:20 +0530

    Update passport config

--- server/config/passport.js
+++ server/config/passport.js
@@ -10,7 +10,7 @@ export default function(passport) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: '/api/auth/google/callback',
+                callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
@@ -52,7 +52,7 @@ export default function(passport) {
             {
                 clientID: process.env.GITHUB_CLIENT_ID,
                 clientSecret: process.env.GITHUB_CLIENT_SECRET,
-                callbackURL: '/api/auth/github/callback',
+                callbackURL: `${process.env.BACKEND_URL}/api/auth/github/callback`,
                 scope: ['user:email'],
             },
             async (accessToken, refreshToken, profile, done) => {
